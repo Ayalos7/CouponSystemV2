645926e494096f9175e57d03a89cee289ca8d93c
Ayal <89256973+Ayalos7@users.noreply.github.com>
2021-11-08 19:05:52 +0200

    Update JwtUtils.java

--- ProjectSpring/src/main/java/Ayal/ProjectSpring/Tokens/JwtUtils.java
+++ ProjectSpring/src/main/java/Ayal/ProjectSpring/Tokens/JwtUtils.java
@@ -16,35 +16,31 @@ import java.util.Map;
 @Service
 
 public class JwtUtils {
-    //type of encryption -
+    //encryption -
     private String signatureAlgorithm = SignatureAlgorithm.HS256.getJcaName();
-    //our private key -
-    private String encodedSecretKey = "this+is+my+key+and+it+must+be+at+least+256+bits+long";
-    //create our private key -
+    
+    private String encodedSecretKey = "****+**+**+***+***+**+****+**+**+*****+***+****+****";
+    
     private Key decodedSecrectKey = new SecretKeySpec(Base64.getDecoder().decode(encodedSecretKey), this.signatureAlgorithm);
 
-    //generate key
-    //we need user email, password and role for create the token
-    //since the userDetail is an instace of class, we need to make it hashcode
-    //the token need to get clamis which is the information in hashcode
     public String generateToken(UserDetails userDetails) {
         Map<String, Object> claims = new HashMap<>();        //create new hash map for claims
-        claims.put("userPass", userDetails.getPassword());  //insert password
+        //claims.put("userPass", userDetails.getPassword());  //insert password
         claims.put("userType", userDetails.getUserType());  //insert user type (role)
         return createToken(claims, userDetails.getEmail()); //send the subject (email)
     }
 
     //we create the JWT token from the information that we got.
     private String createToken(Map<String, Object> claims, String email) {
-        Instant now = Instant.now();//get current time
-        return Jwts.builder()       //create JWT builder to assist us with JWT creation
-                .setClaims(claims)  //set our data (clamis-user,password,role,etc...)
-                .setSubject(email)  //set our subject, the first item that we will check
+        Instant now = Instant.now();
+        return Jwts.builder()       
+                .setClaims(claims)  //set data (clamis-user,password,role,etc...)
+                .setSubject(email)  //set subject
                 .setIssuedAt(Date.from(now))  //create issued at , which is current time
-                //experation date, which after the date, we need to create a new token
+               
                 .setExpiration(Date.from(now.plus(30, ChronoUnit.MINUTES)))
-                .signWith(this.decodedSecrectKey) //sign the token with our secret key
-                .compact();   //compact our token, that it will be smaller :)
+                .signWith(this.decodedSecrectKey) 
+                .compact();   //compact the token
     }
 
     public Claims extractAllClaims(String token) throws ExpiredJwtException {
@@ -79,17 +75,17 @@ public class JwtUtils {
         return (!isTokenExpired(token));
     }
 
-    //tester
+    //tokenTester
     public static void main(String[] args) {
-        //create our instance of user that the token will be created for him.
+        //create an user instance 
         UserDetails admin = new UserDetails("admin@admin.com", "12345", "Admin");
-        //use our new shiny JWTutil.
+        
         JwtUtils myUtil = new JwtUtils();
-        //generate a new token
+        
         String myToken = myUtil.generateToken(admin);
-        //print the token on screen to show to our mother.
+        
         System.out.println("Token:\n" + myToken);
-        //get our claims :)
+        
         System.out.println("Token body:\n" + myUtil.extractAllClaims(myToken));
         System.out.println("email of token:\n" + myUtil.extractEmail(myToken));
     }
